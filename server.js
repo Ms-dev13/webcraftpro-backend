2f35f2baefc38a7a3d5381276e3d1dcba7016663
Ms_13 <sharmamanthan1309@gmail.com>
2025-11-02 23:03:18 +0530

    Add gitignore and secure env

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const mysql = require('mysql2');
 const cors = require('cors');
 const jwt = require('jsonwebtoken');
+require('dotenv').config();
 
 const app = express();
 const port = 5000;
